791a87f6329465c8fc0a81aad49bea8d599ff3ac
NathanielLLally <nathaniel.lally@gmail.com>
2025-01-01 00:46:19 -0500

    finished working

--- parsers/parseURL.pl
+++ parsers/parseURL.pl
@@ -1,18 +1,49 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
 use URI::Find;
 use URI;
+use URI::Escape;
 #use URI::Info;
 use Data::Dumper;
+use Getopt::Long;
 
-use warnings;
-use strict;
+my %url;
+my %urlParts;
+
+#$SIG{__DIE__} = sub {
+#  print "die hook\n";
+  #exit;
+  #};
+
+my ($Pverbose, $Pcount, $Phelp, $Pfile, @Poutput) = ((undef) x 3, '' x 1);
+GetOptions ("count" => \$Pcount, 
+  "file=s" => \$Pfile,
+  "output=s" => \@Poutput,
+  "verbose" => \$Pverbose,
+  "help" => \$Phelp
+);
+
+if (defined $Phelp) {
+ print "output (url,fqdn,query,path,scheme)\n";
+ exit;
+}
+
+my $file = $Pfile;
+my $FD = *STDIN;
+
+if (defined $file and -e "$file") {
+  open($FD, "<$file") || die "cannot open $file";
+}
 
 local $/;
 undef $/;
 
-my $file = <STDIN>;
-my %url;
+my $text = <$FD>;
+#print "$text\n";
+
 
 #my $uriInfo = URI::Info->new();
 my $finder = URI::Find->new(
@@ -20,20 +51,61 @@ my $finder = URI::Find->new(
             my ($uri) = shift;
             #            my $u = URI->new(
             #            my $nfo = $uriInfo->info($uri); 
-            $uri =~ /(.*?)\,/;
-            print "$1\n";
+            #print "$uri\n";
+            #$uri =~ /(.*?)\,/;
+            #print "$1\n";
 
-            $url{$1}++;
+            $url{uri_unescape($uri)}++;
             #            $url{$nfo->host}++;
             #            print $nfo->host."\n";
         }
 );
 
-my $count = $finder->find(\$file);
 
-print "found $count\n";
+print $finder->uri_re."\n".$finder->schemeless_uri_re."\n" if (defined $Pverbose);
 
-foreach my $k(keys %url) {
-  print "$k\n";
+my $count = $finder->find(\$text);
+
+print "output=".Dumper(\@Poutput)."\n" if (defined $Pverbose);
+
+foreach my $k (keys %url) {
+
+  #  if ($k =~ /(https?:\/\/)([\w\-\.]+)((\/.*?)(\?.*?))?/) {
+  #
+  #  see URI pod, section PARSING URIs WITH REGEXP
+  #
+
+     if ($k =~ m|(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*)(?:\?([^#]*))?(?:#(.*))?|) {
+       $urlParts{$k} = {
+         scheme => $1, 
+         authority => $2,  fqdn => $2, 
+         path => $3, query => $4, 
+         fragment => $5, 
+         url => $k
+       };
+       my @parts = split(/\./,$urlParts{$k}{fqdn});
+
+       $urlParts{$k}{host} = shift @parts if ($#parts > 1);
+
+       $urlParts{$k}{domain} = join('.', @parts); 
+       $urlParts{$k}{tld} = $parts[-1];
+     }
+    # ) = ($1,$2,$4,$5);
+    #  print "$scheme, $fqdn, $path, $query\n" if (defined $Pverbose);
 }
 
+print Dumper(\%urlParts) if (defined $Pverbose);
+
+foreach my $k (keys %urlParts) {
+  print "-> $k\n" if (defined $Pverbose);
+  foreach my $el (@Poutput) {
+    if (defined $el and exists $urlParts{$k}{$el}) {
+      print $urlParts{$k}{$el}."\n";
+    }
+  }
+  print "\n";# if (defined $Pverbose);
+}
+
+if (defined $Pcount) {
+  print "found $count\n";
+}
