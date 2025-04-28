d5aa664ee2ad5499672a41753338f77c514a1e88
Nathaniel Lally <nathaniel.lally@gmail.com>
2025-04-27 22:01:51 -0400

    mq upd

--- bin/mailqStats.pl
+++ bin/mailqStats.pl
@@ -22,7 +22,11 @@ my $dbh = DBI->connect($CFG->{dB}->{dsn}, $CFG->{dB}->{user}, $CFG->{dB}->{pass}
 my $bytes = 0;
 
 my $DEBUG = 1;
-my $DRY = 0;
+my $DRY = 1;
+my $arg = shift @ARGV;
+if (defined $arg and $arg =~ /flush/) {
+	$DRY = 0;
+}
 my %dup;
 my %rcptForIds;
 ## Please see file perltidy.ERR
@@ -36,48 +40,58 @@ for my $el (@$entries) {
 }
 
 ## Please see file perltidy.ERR
+my %count = ( pending => 0, dup => 0, defer => 0 );
 foreach my $k (keys %dup) {
-  if ($dup{$k} > 1) {
-    printf "%u mails for %s\n", $dup{$k}, $k;
+	if ($dup{$k} > 0) {
 
-    my $anyError = undef;
-    foreach my $el (@{$rcptForIds{$k}}) {
+		my $anyError = '';
+		foreach my $el (@{$rcptForIds{$k}}) {
 
-      #      printf "%s\t%s\t%s\n", $el->{queue_id}, $el->{date}, $el->{error_string} || "no error" if ($DEBUG);
-      $anyError = $anyError."\t|\t".$el->{error_string} if (defined $el->{error_string});
-    }
+			#      printf "%s\t%s\t%s\n", $el->{queue_id}, $el->{date}, $el->{error_string} || "no error" if ($DEBUG);
+			$anyError = $anyError."\t|\t".$el->{error_string} if (defined $el->{error_string});
+		}
 
+		if ($dup{$k} > 1) {
+			printf "%u mails for %s\n", $dup{$k}, $k;
+			$count{'dup'}++;
+		}
 
+		if (length $anyError > 0) {
+			$count{'defer'}++;
+			if (not $DRY) {
+				print "errors sending to $k\t$anyError\n";
+				my $isth = $dbh->prepare ("update track_email set defer = now() where email = ?");
+				$isth->execute($k);
+				$isth->finish;
+				foreach my $el ( @{ $rcptForIds{$k} } ) {
+					`sudo postsuper -d $el->{queue_id}`;
+				}
+			}
+		} else {
+			$count{'pending'}++;
+			if (not $DRY) {
+				print "no errors, flusing all but first, marking as sent in track_email\n";
+				my $isth = $dbh->prepare ("update track_email set sent = now() where email = ?");
+				$isth->execute($k);
+				$isth->finish;
+				## Please see file perltidy.ERR
+				shift @{ $rcptForIds{$k }};
+				foreach my $el ( @{ $rcptForIds{$k} } ) {
+					`sudo postsuper -d $el->{queue_id}`;
+				}
 
-    if (not $DRY) {
-    if (defined $anyError) {
-      print "errors sending to $k\t$anyError\n";
-        my $isth = $dbh->prepare ("update track_email set defer = now() where email = ?");
-        $isth->execute($k);
-        $isth->finish;
-    foreach my $el ( @{ $rcptForIds{$k} } ) {
-        `sudo postsuper -d $el->{queue_id}`;
-      }
-    } else {
-      print "no errors, flusing all but first, marking as sent in track_email\n";
-        my $isth = $dbh->prepare ("update track_email set sent = now() where email = ?");
-        $isth->execute($k);
-        $isth->finish;
-## Please see file perltidy.ERR
-      shift @{ $rcptForIds{$k }};
-    foreach my $el ( @{ $rcptForIds{$k} } ) {
-        `sudo postsuper -d $el->{queue_id}`;
-      }
-
-    }
-  }
-  } else {
-        my $isth = $dbh->prepare ("select pending,sent from track_email where email = ?");
-        $isth->execute($k);
-        my $rs = $isth->fetchall_arrayref({});
-        foreach (@$rs) {
-          printf "sinqle entry to: %s, db sent: %s error: %s\n", 
-	  $k, $_->{sent}, @{ $rcptForIds{$k} }[0]->{error_string} || "no error";
-        }
-  }
+			}
+		}
+	} else {
+		my $isth = $dbh->prepare ("select pending,sent from track_email where email = ?");
+		$isth->execute($k);
+		my $rs = $isth->fetchall_arrayref({});
+		foreach (@$rs) {
+			printf "sinqle entry to: %s, db sent: %s error: %s\n", 
+			$k, $_->{sent}, @{ $rcptForIds{$k} }[0]->{error_string} || "no error";
+		}
+	}
 }
+
+printf "duplicates: %u\tto be defered: %u\tstill pending: %u\n", 
+map { $count{$_} } qw/dup defer pending/;
