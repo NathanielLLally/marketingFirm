4bbbe1cd4b6091f725abe5c3bb331e435b0567a0
NathanielLLally <nathaniel.lally@gmail.com>
2025-06-05 15:18:31 -0400

    simple mail sender like sendmail

---
+++ bin/smtp.pl
@@ -0,0 +1,27 @@
+#/usr/bin/perl
+use warnings;
+use strict;
+
+use Email::Sender::Simple qw(sendmail);
+use Email::Sender::Transport::SMTP qw();
+use Try::Tiny;
+
+my ($host,$port) = @ARGV;
+
+local $/;
+my $message = <STDIN>;
+
+try {
+    sendmail(
+        $message,
+        {
+            transport => Email::Sender::Transport::SMTP->new({
+                    host => $host,
+                    port => $port,
+                })
+        }
+    );
+} catch {
+    warn "sending failed: $_";
+};
+
